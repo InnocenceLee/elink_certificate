ae03b49db81dcddf530cca6bfe50b794076b16dd
lijiangdada <lijiangdada1991@163.com>
2018-05-07 17:28:36 +0800

    init

---
+++ src/main/java/com/elink/certificate/ElinkCertificateApplication.java
@@ -0,0 +1,12 @@
+package com.elink.certificate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElinkCertificateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElinkCertificateApplication.class, args);
+	}
+}
